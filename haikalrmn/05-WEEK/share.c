e64e4b7014b66fc61c373ca11de6df805403e185
haikalrmn <haikalrmn@haikalrmn.myguest.virtualbox.org>
2023-03-13 16:12:53 +0700

    4th 15 minute interval

--- haikalrmn/05-WEEK/share.c
+++ haikalrmn/05-WEEK/share.c
@@ -1,15 +1,13 @@
+// REV02: Mon 13 Mar 2023 16:12
 // REV01: Sun 12 Mar 2023 18:00
 // START: Sat 11 Mar 2023 22:00
 // Please complete this file.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "share.h"
 
-char )
+void dummy()
 {  
-  char GITHUB_ACCOUNT[10] = "haikalrmn";
-  sprintf("%s", GITHUB_ACCOUNT);
-  
-  return 0;
+  printf("%s\n", GITHUB_ACCOUNT);
 }
-
