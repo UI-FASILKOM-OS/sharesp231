89427556489a81fb0131f3680988128cc45834a4
Bintang Hari Pratama <79510565+bintangbhp@users.noreply.github.com>
2023-03-13 16:01:55 +0700

    add GITHUB_ACCOUNT

--- bintangbhp/05-WEEK/share.h
+++ bintangbhp/05-WEEK/share.h
@@ -2,4 +2,13 @@
 // START: Sat 11 Mar 2023 22:00
 // Please complete this file.
 
+#ifndef SHARE_H
+#define SHARE_H
+
+// Declaration of the dummy function
 void dummy();
+
+// Definition of the GITHUB_ACCOUNT macro
+#define GITHUB_ACCOUNT "bintangbhp"
+
+#endif
