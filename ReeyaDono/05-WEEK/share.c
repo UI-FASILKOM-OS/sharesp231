a3ebd75d60127c974c1b12d0c4f15ca7782aa50a
ReeyaDono <metrytmc@gmail.com>
2023-03-13 15:53:08 +0700

    Update more

--- ReeyaDono/05-WEEK/share.c
+++ ReeyaDono/05-WEEK/share.c
@@ -1,7 +1,9 @@
 // REV01: Sun 12 Mar 2023 18:00
 // START: Sat 11 Mar 2023 22:00
 // Please complete this file.
+#include "share.h"
+#include <stdio.h>
 
 void dummy(){
-    printf("%s", GITHUB_ACCOUNT)
+    printf("%s\n", GITHUB_ACCOUNT);
 }
