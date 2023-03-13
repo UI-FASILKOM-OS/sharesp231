d264e70454cb6c7b4136fd1e84db4656b3c48364
Adillah Putri <70618765+adillahptr@users.noreply.github.com>
2023-03-13 16:06:40 +0700

    Edit share.h

--- adillahptr/05-WEEK/share.h
+++ adillahptr/05-WEEK/share.h
@@ -1,5 +1,10 @@
 // REV01: Sun 12 Mar 2023 18:00
 // START: Sat 11 Mar 2023 22:00
 // Please complete this file.
+#ifndef SHARE_H
+#define SHARE_H
 
-char GITHUB_ACCOUNT[] = "adillahptr";
+#define GITHUB_ACCOUNT "adillahptr"
+void dummy();
+
+#endif
